a0c0fcc4352ed50df76405243954a2fa435e3660
baptiste <baptiste.leroyer@epitech.eu>
2022-02-21 15:45:25 +0100

    [FIX] tkt ca marche pas mais ca va etre réparé

---
+++ test1/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2022
+** test1
+** File description:
+** Main file of minishell2 project.
+*/
+
+int main(int argc, char **argv)
+{
+    return minishell2(argc, argv);
+}
---
+++ test1/src/minishell2.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2022
+** test1
+** File description:
+** Main file of minishell2 project.
+*/
+
+int minishell2(int argc, char **argv)
+{
+    return (0);
+}
